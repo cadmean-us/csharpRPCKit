1263b2180ea06bb310ef86541510cd5f4dd97ad3
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2020-10-16 17:41:22 +0300

    Add example server and cli projects

---
+++ Cadmean.RPC.CLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadmean.RPC.CLI
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                await LaunchInteractiveMode();
+            }
+            else if (args.Length < 3)
+            {
+                Console.WriteLine("At least two arguments requires - server url and function name");
+            }
+            else
+            {
+                var serverName = args[0];
+                var functionName = args[1];
+                var functionArgs = args.Skip(2).ToArray();
+                await CallSender.Send(serverName, functionName, functionArgs);
+            }
+        }
+
+
+        private static async Task LaunchInteractiveMode()
+        {
+            Console.Write("Enter server url: ");
+            var serverName = Console.ReadLine();
+            Console.Write("Enter function name: ");
+            var functionName = Console.ReadLine();
+            Console.Write("Enter function arguments: ");
+            var args = Console.ReadLine()?.Split(' ');
+            await CallSender.Send(serverName, functionName, args);
+            Console.WriteLine("Bye!");
+        }
+    }
+}
---
+++ ExampleRPCServer/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cadmean.RPC.ASP;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleRPCServer.Controllers
+{
+    [ApiController]
+    [Route("api/rpc/weatherForecast.get")]
+    public class WeatherForecastController : FunctionController
+    {
+        private static readonly string[] Summaries = 
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> OnCall()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
