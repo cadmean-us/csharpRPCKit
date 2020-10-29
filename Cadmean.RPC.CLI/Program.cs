1f28f3f93c1c683cbb8cd7f9480dadb65e849857
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2020-10-29 19:13:44 +0300

    Update cli

--- Cadmean.RPC.CLI/Program.cs
+++ Cadmean.RPC.CLI/Program.cs
@@ -6,38 +6,97 @@ namespace Cadmean.RPC.CLI
 {
     public static class Program
     {
+        private static bool repeat;
+        
+        private static string serverName = "";
+        private static string functionName = "";
+        private static string[] functionArgs;
+        
         public static async Task Main(string[] args)
         {
             if (args.Length == 0)
             {
                 await LaunchInteractiveMode();
             }
-            else if (args.Length < 3)
-            {
-                Console.WriteLine("At least two arguments requires - server url and function name");
-            }
             else
             {
-                var serverName = args[0];
-                var functionName = args[1];
-                var functionArgs = args.Skip(2).ToArray();
-                await CallSender.Send(serverName, functionName, functionArgs);
+                int i = 0;
+                int c = 0;
+
+                foreach (var arg in args)
+                {
+                    switch (arg)
+                    {
+                        case "-r":
+                            repeat = true;
+                            break;
+                        default:
+                            switch (c)
+                            {
+                                case 0:
+                                    serverName = arg switch
+                                    {
+                                        "local" => "http://localhost:5000",
+                                        "test" => "http://testrpc.cadmean.ru",
+                                        _ => arg,
+                                    };
+                                    break;
+                                case 1:
+                                    functionName = arg;
+                                    break;
+                                default:
+                                    functionArgs = args.Skip(i).ToArray();
+                                    break;
+                            }
+
+                            c++;
+                            break;
+                    }
+
+                    i++;
+                }
+
+                if (c < 2)
+                    await LaunchInteractiveMode();
+                else
+                    await CallSender.Send(serverName, functionName, functionArgs);
             }
         }
 
 
         private static async Task LaunchInteractiveMode()
         {
-            Console.Write("Enter server url: ");
-            var serverName = Console.ReadLine();
-            if (serverName == "")
-                serverName = "http://localhost:5000";
-            Console.Write("Enter function name: ");
-            var functionName = Console.ReadLine();
-            Console.Write("Enter function arguments: ");
-            var args = Console.ReadLine()?.Split(' ');
-            await CallSender.Send(serverName, functionName, args);
-            Console.WriteLine("Bye!");
+            while (true)
+            {
+                if (string.IsNullOrEmpty(Program.serverName))
+                {
+                    Console.Write("Enter server url: ");
+                    var sn = Console.ReadLine();
+                    serverName = sn switch
+                    {
+                        "" => "http://localhost:5000",
+                        "test" => "http://testrpc.cadmean.ru",
+                        _ => sn
+                    };
+                }
+
+                if (string.IsNullOrEmpty(functionName))
+                {
+                    Console.Write("Enter function name: "); 
+                    functionName = Console.ReadLine();
+                }
+
+                Console.Write("Enter function arguments: ");
+                var args = Console.ReadLine()?.Split(' ');
+                await CallSender.Send(serverName, functionName, args);
+
+                if (repeat)
+                {
+                    functionName = "";
+                    continue;
+                }
+                break;
+            }
         }
     }
 }
