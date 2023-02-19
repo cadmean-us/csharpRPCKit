68a396e241c34f85266f7a7d6b19e0d50f4a8b29
Aleksei Kritskov <kritskov@cadmean.dev>
2023-02-20 03:43:46 +0700

    Updated project and RpcAssert

--- Cadmean.RPC.ASP/Helpers/RpcAssert.cs
+++ Cadmean.RPC.ASP/Helpers/RpcAssert.cs
@@ -4,6 +4,21 @@ namespace Cadmean.RPC.ASP.Helpers;
 
 public static class RpcAssert
 {
+    public static void IsTrue<TException>(bool condition) where TException : FunctionException, new()
+    {
+        if (condition)
+        {
+            return;
+        }
+
+        throw new TException();
+    }
+
+    public static void IsTrue<TException>(Func<bool> condition) where TException : FunctionException, new()
+    {
+        IsTrue<TException>(condition());
+    }
+    
     public static void IsTrue(bool condition, string error)
     {
         if (condition)
